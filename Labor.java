33a67cac8c557c6bd37aebc3ae098c554d88b0a9
CEC <learnmorececc1@gmail.com>
2025-07-17 11:51:50 +0200

    Kommentare hinzugefügt.

--- Labor.java
+++ Labor.java
@@ -1,11 +1,14 @@
 public class Labor 
 {
-    private String gebaude;
-    private int stock;
-    private boolean beamer;
-    private int platze;
+    private String gebaude; // In welche Gebaude der Labor sich befindet
+    private int stock;      // In welcher Stock der Labor sich befindet
+    private boolean beamer; // Ob der Labor beamer hat oder nicht (Ja oder Nein)
+    private int platze;     // Wie vile Plätze im Labor gibt
     
     //Constructor
+    /*
+     * It is creating a new Instance of the class Labor.
+     */
     public Labor(String newGebaude, int newStock, boolean newBeamer, int newPlatze)
     {
         setGebaude(newGebaude);
@@ -13,23 +16,28 @@ public class Labor
         setBeamer(newBeamer);
         setPlatze(newPlatze);
     }
+    
     public void setGebaude(String newGebaude)
     {
+        // set the property gebaude to the value of the parameter newGebaude.
         gebaude = newGebaude;
     }
     
     public void setStock(int newStock)
     {
+        // set the property stock to the value of the parameter newStock.
         stock = newStock;
     }
     
     public void setBeamer(boolean newBeamer)
     {
+        // set the property beamer to the value of the newBeamer.
         beamer = newBeamer;
     }
     
     public void setPlatze(int newPlatze)
     {
+        // set the property platze to the value of the newPlatze.
         platze = newPlatze;
     }
     
